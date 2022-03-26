be358d1f01b31e3e776b6cab3aabb51106ba6a72
Good Juju <92497463+Good-Juju935@users.noreply.github.com>
2022-03-26 20:16:18 +0300

    Updated scripts.js
    The script is now able to know how many pages exist in total

--- unusual-necromancer/scripts.js
+++ unusual-necromancer/scripts.js
@@ -1,13 +1,36 @@
 var currentPage = 1;
 var minPage = 1;
-var maxPage = 3;
+var maxPage = 1;
 
 var cPath = ''
 
 window.onload = function() {
     read_file()
-    for (i = 1; i <= maxPage ;i++) {
-        console.log(i)
+
+    for(var i = 1;i < 101;i++) {
+        var dirs = "chapters/c" + i + ".txt";
+        var temp = maxPage;
+        var getSize = new XMLHttpRequest();
+        getSize.open("GET", dirs, false)
+        getSize.onload = function ()
+        {
+            if (getSize.readyState === 4) {
+                if (getSize.status === 404) {
+                    console.log("Max Pages Set To " + (maxPage - 1))
+                } else {
+                    maxPage++;
+                }
+            }
+        }
+        getSize.send(null);
+        
+        if (temp == maxPage) {
+            maxPage--;
+            break;
+        }
+    }
+
+    for (var i = 1; i <= maxPage ;i++) {
         document.getElementById("pagesDrop").innerHTML += "<li><a class=\"dropdown-item\" onclick=\"rf(" + i + ")\">" + i + "</a></li>"
     }
 };
@@ -27,7 +50,6 @@ function read_file() {
             if(rawFile.status === 200 || rawFile.status == 0)
             {
                 var allText = rawFile.responseText;
-                console.log(allText);
                 var splitText = allText.split("\n");
                 splitText[0] = "<H1>" + splitText[0] + "</H1>";
                 for (i = 1; i < splitText.length; i++) {
@@ -36,9 +58,12 @@ function read_file() {
                     splitText[i] = splitText[i].replace(/`/g,'')
                 }
                 var out = splitText.join('')
-                console.log(splitText)
                 document.getElementById('Story-Content').innerHTML = out;
             }
+        }
+
+        if(rawFile.status === 404) {
+            maxPage = currentPage - 1;
         }
     }
     rawFile.send(null);
